18643d8ee4429e04f6bfb009ce5a4ee497b2c64a
HedgeComp <94635857+HedgeComp@users.noreply.github.com>
2025-02-05 11:12:50 -0500

    Update ColourV2.ps1
    Add some Selected Colours

--- FFUDevelopment/Output/ColourV2.ps1
+++ FFUDevelopment/Output/ColourV2.ps1
@@ -1,1 +1,69 @@
+function Get-PCInfo {
+    [CmdletBinding()]
+    param()
 
+    Write-Host "--- Retrieving Computer Manufacturer, Model, and Processor ---" -ForegroundColor Cyan
+
+    # Attempt to retrieve Win32_ComputerSystem
+    try {
+        $computerSystemCIM = Get-CimInstance -ClassName Win32_ComputerSystemProduct -ErrorAction Stop
+       
+    } catch {
+        Write-Host "Failed to retrieve Win32_ComputerSystem" -ForegroundColor Red
+        $computerSystemCIM = $null
+    }
+
+    # Attempt to retrieve Win32_Processor
+    try {
+        $processorCIM = Get-CimInstance -ClassName Win32_Processor -ErrorAction Stop
+        }
+  catch {
+        Write-Host "Failed to retrieve Win32_Processor" -ForegroundColor Red
+        $processorCIM = $null
+    }
+
+    # Attempt to retrieve Memory
+    try {
+        $physicalMemory = Get-CimInstance -ClassName Win32_PhysicalMemory -ErrorAction Stop
+        $totalMemoryGB = ($physicalMemory.Capacity | Measure-Object -Sum).Sum / 1GB
+        } catch {
+        Write-Host "Failed to retrieve Win32_PhysicalMemory" -ForegroundColor Red
+        $totalMemoryGB = 0
+    }
+
+    # If the objects are null, output 'Undetermined'
+    $name         = if ($computerSystemCIM) { $computerSystemCIM.Name    } else { "Undetermined" }
+    $manufacturer = if ($computerSystemCIM) { $computerSystemCIM.Vendor  } else { "Undetermined" }
+    $model        = if ($computerSystemCIM) { $computerSystemCIM.Version } else { "Undetermined" }
+    $processor    = if ($processorCIM)      { $processorCIM.Name        } else { "Undetermined" }
+
+    cls
+    Write-Host "-------- DEVICE INFORMATION --------" -ForegroundColor White -BackgroundColor DarkGray
+
+    Write-Host " Make:" -BackgroundColor Gray -ForegroundColor White -NoNewLine
+    Write-Host "  $manufacturer " -ForegroundColor White -BackgroundColor DarkBlue
+
+    # If Vendor is not Lenovo use $name is the Vendors Model
+    if ($manufacturer -ne 'Lenovo') {
+        # If Version is blank, show 'Name:' line with $model, 'Model:' line with $name
+        Write-Host " Model:" -BackgroundColor gray -ForegroundColor White -NoNewLine
+        Write-Host " $name " -ForegroundColor White -BackgroundColor darkblue
+    } else {
+        # Lenovo Devices Use Machine Type for Name and Version for Model
+        Write-Host " Model:" -BackgroundColor gray -ForegroundColor White -NoNewLine
+        Write-Host " $model " -ForegroundColor White -BackgroundColor darkblue
+
+        Write-Host " Machine Type:" -BackgroundColor gray -ForegroundColor White -NoNewLine
+        Write-Host " $name " -ForegroundColor White -BackgroundColor darkblue
+    }
+
+    Write-Host " Processor:" -BackgroundColor gray -ForegroundColor White -NoNewLine
+    Write-Host " $processor " -ForegroundColor White -BackgroundColor blue
+
+    Write-Host " Memory:" -BackgroundColor gray -ForegroundColor White -NoNewLine
+    Write-Host " $totalMemoryGB GB " -ForegroundColor White -BackgroundColor blue
+}
+
+
+
+get-pcinfo
